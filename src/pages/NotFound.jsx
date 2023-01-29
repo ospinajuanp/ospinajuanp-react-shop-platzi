e29aa3d8eaf86438bfe0681ea1654f12fbbfd04d
Ospinajuanp <juanpo996@gmail.com>
2023-01-29 02:03:21 -0500

    Header en la pagina home

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -5,7 +5,7 @@ const NotFound = () => {
     return (
         <div className='container__notFound'>
             <div className='container__notFound-content'>
-                <h2>404</h2>
+                <h2>404!</h2>
                 <h4>Ops! Page not found</h4>
                 <p>The page you were looking for doesn't exist. You may have mistyped the address or the page may have moved</p>
             </div>
